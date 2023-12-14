daf667a6d8423d72eb088400eeee2c7d1fd45a31
deima24 <>
2023-12-14 21:19:09 +0000

    added env.py and procfile

--- BikeRide/settings.py
+++ BikeRide/settings.py
@@ -11,21 +11,25 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-x$b-&)6f3to6kd954xh*-s0vxzm3-hngf5_dv_#@)snz%l#_uj"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-deima24-ride-v3-9qnyszvbs3.us2.codeanyapp.com']
+ALLOWED_HOSTS = ['8000-deima24-ride-v3-9qnyszvbs3.us2.codeanyapp.com', 'bike-ride-v3.herokuapp.com']
 
 
 # Application definition
@@ -36,8 +40,12 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    "cloudinary_storage",
     "django.contrib.staticfiles",
+    "cloudinary",
     "Ride",
+    
+
 ]
 
 MIDDLEWARE = [
@@ -55,7 +63,7 @@ ROOT_URLCONF = "BikeRide.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [TEMPLATES_DIR],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -116,7 +124,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
---
+++ env.py
@@ -0,0 +1,6 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgres://nbmrrkjj:Ap_8tTnKU9HPmIK7Pq6RE1lE7UzYzfei@surus.db.elephantsql.com/nbmrrkjj"
+os.environ["SECRET_KEY"] = "randomkey123..."
+
+os.environ["CLOUDINARY_URL"] = "cloudinary://162114844479885:fALK_L0tEfdXmDtFCkoeaGagXqY@dmecv9zwa"
