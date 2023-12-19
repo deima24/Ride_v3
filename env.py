364f7db451ae5f2a0ec59d86464a57e916710178
deima24 <>
2023-12-19 00:00:51 +0000

    added allauth templates

--- env.py
+++ env.py
@@ -1,6 +1,6 @@
 import os
 
-os.environ["DATABASE_URL"] = "postgres://nbmrrkjj:Ap_8tTnKU9HPmIK7Pq6RE1lE7UzYzfei@surus.db.elephantsql.com/nbmrrkjj"
+os.environ["DATABASE_URL"] = "postgres://qtvchery:cL_ksK1Tpb81aNsWCgtB0HnKarAiYzFE@surus.db.elephantsql.com/qtvchery"
 os.environ["SECRET_KEY"] = "randomkey123..."
 
 os.environ["CLOUDINARY_URL"] = "cloudinary://162114844479885:fALK_L0tEfdXmDtFCkoeaGagXqY@dmecv9zwa"
--- ride/migrations/0001_initial.py
+++ ride/migrations/0001_initial.py
@@ -95,7 +95,7 @@ class Migration(migrations.Migration):
                     models.ForeignKey(
                         on_delete=django.db.models.deletion.CASCADE,
                         related_name="comments",
-                        to="Ride.entry",
+                        to="ride.entry",
                     ),
                 ),
             ],
